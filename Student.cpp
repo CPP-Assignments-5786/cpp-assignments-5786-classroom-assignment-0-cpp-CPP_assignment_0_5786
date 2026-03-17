8d0b8a176bf66acc071c431c707ff9a276925313
Erel Segal-Halevi <erelsgl@gmail.com>
2026-03-17 20:10:57 +0200

    submit with a bug

--- Student.cpp
+++ Student.cpp
@@ -1,6 +1,9 @@
 #include "Student.hpp"
+#include <format>
+using namespace std;
 
 std::string greet(const std::string& name) {
     // TODO: return the greeting message for the given name
-    return ""; // REPLACE THIS LINE
+    int a = 5;
+    return format("Welcome to CPP course, {}!", name); // REPLACE THIS LINE
 }
