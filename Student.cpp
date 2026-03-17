19b86574694318649d5fc3f4d68d9ed8545a4e2c
Oded Rahmani <oded1243@gmail.com>
2026-03-18 01:13:50 +0200

    Submit

--- Student.cpp
+++ Student.cpp
@@ -2,5 +2,5 @@
 
 std::string greet(const std::string& name) {
     // TODO: return the greeting message for the given name
-    return "Welcome to CPP course, " + name + "!"; // REPLACE THIS LINE
+    return "Welcome to CPP course, " + name + "!";
 }
