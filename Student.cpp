7086672d3a763f2179947d13705aa6ab8f1ebea5
eliyahuri <tkhvu1236@gmail.com>
2026-03-18 10:37:59 +0200

    solved

--- Student.cpp
+++ Student.cpp
@@ -1,6 +1,7 @@
 #include "Student.hpp"
 
-std::string greet(const std::string& name) {
+std::string greet(const std::string &name)
+{
     // TODO: return the greeting message for the given name
-    return ""; // REPLACE THIS LINE
+    return "Welcome to CPP course, " + name + "!"; // REPLACE THIS LINE
 }
